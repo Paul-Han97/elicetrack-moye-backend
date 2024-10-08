34d423b25728cf5a7cbad2452e7992fc302e01c6
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-10-08 17:47:39 +0900

    🚧 chore: .prettierrc 적용

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
 export default {
-    server: {
-        port: process.env.SERVER_PORT
-    },
-    database: {
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        name: process.env.DATABASE_NAME
-    }
-}
+  server: {
+    port: process.env.SERVER_PORT,
+  },
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+  },
+};
--- src/db/datasource.ts
+++ src/db/datasource.ts
@@ -1,21 +1,21 @@
-import { DataSource } from "typeorm";
-import config from "../config";
+import { DataSource } from 'typeorm';
+import config from '../config';
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: config.database.host,
-    port: Number(config.database.port),
-    username: config.database.username,
-    password: config.database.password,
-    entities: ["src/entities/*"],
-    logging: true,
-    synchronize: true,
+  type: 'mysql',
+  host: config.database.host,
+  port: Number(config.database.port),
+  username: config.database.username,
+  password: config.database.password,
+  entities: ['src/entities/*'],
+  logging: true,
+  synchronize: true,
 });
 
 AppDataSource.initialize()
-    .then(() => {
-        console.log("AppDataSource 초기화");
-    })
-    .catch((e) => {
-        console.error(`AppDataSource 초기화 중 오류: ${e}`);
-    });
+  .then(() => {
+    console.log('AppDataSource 초기화');
+  })
+  .catch((e) => {
+    console.error(`AppDataSource 초기화 중 오류: ${e}`);
+  });
