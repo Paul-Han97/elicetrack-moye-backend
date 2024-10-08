db9a5129677e640a92fbd364d62831722c24eab6
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-10-08 17:20:38 +0900

    🐓 style: 코드 컨벤션 적용

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,41 +1,46 @@
 import { NextFunction, Request, Response } from "express";
 
-const messageMap: {[key:number]:string} = {
-    // Successful responses
-    200:"OK",
-    201:"Created",
-    202:"Accepted",
-    204:"No Content",
+const messageMap: { [key: number]: string } = {
+  // Successful responses
+  200: "OK",
+  201: "Created",
+  202: "Accepted",
+  204: "No Content",
 
-    // Client error responses
-    400:"Bad Request",
-    401:"Unauthorized",
-    403:"Forbidden",
-    404:"Not Found",
-    
-    // Server error responses
-    500:"Internal Server Error",
-    505:"HTTP Version Not Supported"
-}
+  // Client error responses
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
 
-export function errorHandler(e:Error, req:Request, res:Response, next:NextFunction) {
-    if(e.name !== 'Error') {
-        res.status(500);
-    }
-    
-    const statusCode = res.statusCode;
-    
-    if(statusCode === 500) {
-        console.log(e.stack);
-    }
+  // Server error responses
+  500: "Internal Server Error",
+  505: "HTTP Version Not Supported",
+};
 
-    const statusMessage = messageMap[statusCode];
-    const errorMessage = e.message;
+export function errorHandler(
+  e: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (e.name !== "Error") {
+    res.status(500);
+  }
 
-    const status = {
-        status_code: statusCode,
-        message: `${statusMessage} ${errorMessage}`,
-    }
-    
-    return res.send(status);
+  const statusCode = res.statusCode;
+
+  if (statusCode === 500) {
+    console.log(e.stack);
+  }
+
+  const statusMessage = messageMap[statusCode];
+  const errorMessage = e.message;
+
+  const status = {
+    status_code: statusCode,
+    message: `${statusMessage} ${errorMessage}`,
+  };
+
+  return res.send(status);
 }
