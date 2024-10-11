221766870f54ac792846ad983adc5a6e384a61e3
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-10-11 18:51:34 +0900

    🐣 feat: 회원가입 signup 추가

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Response } from 'express';
 import { userService } from '../services/user.service';
+import { IUser } from '../interface/user.interface';
 
 class UserController {
   async findByEmail(req: any, res: Response, next: NextFunction) {
@@ -19,6 +20,18 @@ class UserController {
       const { id } = req.params;
 
       const result = await userService.findByIdWithRole(id);
+
+      res.status(200).send({ body: result });
+    } catch (e) {
+      next(e);
+    }
+  }
+  
+  async signup(req: any, res: Response, next: NextFunction) {
+    try {
+      const userDto:IUser = req.body;
+
+      const result = await userService.signup(userDto);
 
       res.status(200).send({ body: result });
     } catch (e) {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,11 @@
+import { CREDENTIAL_TYPE, ROLE_TYPE } from '../constants';
+import { CredentialType } from '../entities/credential-type.entity';
+import { Credential } from '../entities/credential.entity';
+import { Role } from '../entities/role.entity';
+import { UserCredential } from '../entities/user-credential.entity';
+import { User } from '../entities/user.entity';
 import { IGenerate } from '../interface/token.interface';
+import { ISignup, IUser } from '../interface/user.interface';
 import { userRepository } from '../repositories/user.repository';
 import { Encrypt } from '../utils/encrypt.util';
 import { serverMessage, statusMessage } from '../utils/message.util';
@@ -40,6 +47,45 @@ class UserService {
       refresh,
     };
     
+    return result;
+  }
+
+  async signup({email, name, phone, password}:IUser) {
+    const hasUser = await userRepository.findByEmail(email);
+
+    if(hasUser) {
+      const msg = `${statusMessage.BAD_REQUEST}+이미 계정이 존재합니다.`;
+      throw new Error(msg);
+    }
+
+    const user = new User();
+    user.email = email;
+    user.name = name;
+    user.phone = phone;
+
+    const credentialType = new CredentialType();
+    credentialType.id = CREDENTIAL_TYPE.LOCAL_ID
+
+    const role =  new Role();
+    role.id = ROLE_TYPE.MANAGER_ID;
+
+    const credential = new Credential();
+    credential.password = Encrypt.encode(password);
+    credential.credentialType = credentialType;
+    credential.role = role;
+
+    const userCredential = new UserCredential();
+    userCredential.user = user;
+    userCredential.credential = credential;
+
+    const signupDto:ISignup = {
+      user,
+      credential,
+      userCredential
+    }
+
+    const result = userRepository.signup(signupDto);
+
     return result;
   }
 }
