62ebac8c5fb4217be3363fabf67cf776b2cef5dd
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-10-14 20:34:57 +0900

    🍳 refactor: 상수를 enum 타입으로 변경
    https://kdt-gitlab.elice.io/react-node-track/class01/web-project1/team01/backend/-/merge_requests/24#note_208479

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,32 +1,32 @@
-export const CREDENTIAL_TYPE = {
-  LOCAL_ID: 1,
-};
+export const enum CREDENTIAL_TYPE {
+  LOCAL_ID = 1,
+}
 
-export const ROLE_TYPE = {
-  USER_ID: 1,
-  MANAGER_ID: 2,
-  ADMIN_ID: 3,
-};
+export const enum ROLE_TYPE {
+  USER_ID = 1,
+  MANAGER_ID = 2,
+  ADMIN_ID = 3,
+}
 
-export const RESERVATION_TYPE = {
-  ACCEPT: 'ACCEPT',
-  PENDING: 'PENDING',
-  CANCEL: 'CANCEL',
-};
+export const enum RESERVATION_TYPE {
+  ACCEPT = 'ACCEPT',
+  PENDING = 'PENDING',
+  CANCEL = 'CANCEL',
+}
 
 export const PAGINATION_LIMIT = 5;
 
 export const WEEK = 7;
 
-export const WEEK_TYPE = {
-  SUN: 1,
-  MON: 2,
-  TUE: 3,
-  WED: 4,
-  THU: 5,
-  FRI: 6,
-  SAT: 7,
-};
+export const enum WEEK_TYPE {
+  SUN = 1,
+  MON = 2,
+  TUE = 3,
+  WED = 4,
+  THU = 5,
+  FRI = 6,
+  SAT = 7,
+}
 
 export const WEEKDAYS_TYPE = '평일';
 
