68eb618113e520eb7f72c8f0ed940e20ffb93088
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-10-14 17:58:24 +0900

    🐣 feat: 평일, 주말 관련 상수 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,3 +15,19 @@ export const RESERVATION_TYPE = {
 };
 
 export const PAGINATION_LIMIT = 5;
+
+export const WEEK = 7;
+
+export const WEEK_TYPE = {
+  SUN: 1,
+  MON: 2,
+  TUE: 3,
+  WED: 4,
+  THU: 5,
+  FRI: 6,
+  SAT: 7,
+};
+
+export const WEEKDAYS_TYPE = '평일';
+
+export const WEEKEND_TYPE = '주말';
