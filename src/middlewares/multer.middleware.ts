2c8cbb9c72cffa03e87834fadb2390919755a6b4
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-10-18 13:47:43 +0900

    🐣 feat: entity별로 폴더 생성하도록 수정

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -3,6 +3,9 @@ import { Request } from 'express';
 import multer from 'multer';
 import { fileUtil } from '../utils/file.util';
 import { serverMessage, statusMessage } from '../utils/message.util';
+import { STORE_PATH, USER_PATH } from '../constants';
+
+const ENTITY_INDEX = 2;
 
 const fileFilter = (
   req: Request,
@@ -12,7 +15,15 @@ const fileFilter = (
   const extname = fileUtil.getFileExtname(file);
 
   if (extname.toLowerCase() !== fileUtil.JPG) {
-    const msg = `${statusMessage.BAD_REQUEST}+${serverMessage.E007}`;
+    const msg = `${statusMessage.UNSUPPORTED_MEDIA_TYPE}+${serverMessage.E007}`;
+    return cb(new Error(msg));
+  }
+
+  const baseUrl = req.baseUrl;
+  const entity = baseUrl.split('/')[ENTITY_INDEX];
+
+  if (entity !== USER_PATH && entity !== STORE_PATH) {
+    const msg = `${statusMessage.BAD_REQUEST}+${serverMessage.E001}`;
     return cb(new Error(msg));
   }
 
@@ -22,19 +33,18 @@ const fileFilter = (
 const storage = multer.diskStorage({
   filename(req, file, cb) {
     const extname = fileUtil.getFileExtname(file);
-
     const filename = `${uuidv6()}${extname}`;
 
-    file.originalname = filename;
-
-    cb(null, file.originalname);
+    cb(null, filename);
   },
   destination(req, file, cb) {
-    const { storeId } = req.params;
+    const baseUrl = req.baseUrl;
+    const entity = baseUrl.split('/')[ENTITY_INDEX];
+    const dir = fileUtil.join(fileUtil.cwd, entity);
 
-    fileUtil.mkdir(storeId);
+    fileUtil.mkdir(dir);
 
-    cb(null, fileUtil.getWorkingDir(storeId));
+    cb(null, dir);
   },
 });
 
