6c5132d7f72aff14d80518661a3647ef95b011ed
[Node1] 한바울 <wtg.paulhan@gmail.com>
2024-10-18 17:44:34 +0900

    🩹 fix: 요청 버그 수정

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { userController } from '../controllers/user.controller';
 import upload from '../middlewares/multer.middleware';
+import { auth } from '../middlewares/auth.middleware';
 const router = Router();
 
 router.get('/', userController.findByEmail);
 router.get('/:id', userController.findByIdWithRole);
 router.post('/', userController.signup);
-router.put('/:id', upload, userController.updateImage);
+router.put('/:id/uploads', auth, upload, userController.updateImage);
 
 export const userRouter = router;
